6f52843b64f5555089ea7ff707c5cfbf114e8cf2
mino4084 <mino4084@gmail.com>
2017-08-11 10:45:34 +0900

    여행 생성

--- models/trip.js
+++ models/trip.js
@@ -19,30 +19,9 @@ var TripSchema = new Schema({
     trip_list : [ Candidate ]
 });
 
-TripSchema.virtual('start_date')
-	.get(function(){
-		return formatDate(this.start_date);
-	});
-TripSchema.virtual('end_date')
-	.get(function(){
-		return formatDate(this.end_date);
-	});
-
 TripSchema.set('toJSON', { virtuals : true });
 
 TripSchema.plugin(autoIncrement.plugin, { model : 'Trip', field : 'trip_no', startAt : 1, incrementBy : 1});
 
 var Trip = db.model('Trip', TripSchema);
 module.exports = Trip;
-
-function formatDate(date){
-	var y = date.getFullYear();
-	var m = date.getMonth() + 1;
-	var d = date.getDate();
-	var h = date.getHours();
-	var i = date.getMinutes();
-	var s = date.getSeconds();
-	// yyyy-MM-dd hh:mm:ss 형태
-	var day = y + '-' + (m > 9 ? m : "0" + m) + '-' + (d > 9 ? d : "0" + d) + ' ' + (h > 9 ? h : "0" + h) + ':' + (i > 9 ? i : "0" + i) + ':' + (s > 9 ? s : "0" + s);
-	return day;
-};
