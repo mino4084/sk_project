f0295ec9dbfcac1efe673336bf6ee3725e5505bd
mino4084 <mino4084@gmail.com>
2017-08-11 16:43:04 +0900

    여행 삭제

--- routes/index.js
+++ routes/index.js
@@ -362,7 +362,7 @@ router.post('/update_trip', function(req, res, next){
 	});
 });
 
-router.get('/remove_trip/:trip_no', function(req, res, next){
+router.get('/delete_trip/:trip_no', function(req, res, next){
 	var trip_no = req.params.trip_no;
 	console.log('trip_no =', trip_no);
 	TripModel.findOne({trip_no : trip_no}, function(err, doc){
@@ -373,13 +373,48 @@ router.get('/remove_trip/:trip_no', function(req, res, next){
 
 		console.log('doc =', doc); // 실패할 경우 null
 		if(doc){
-			res.render('remove_trip', {title : "remove_trip", doc : doc});
+			res.render('delete_trip', {title : "delete_trip", doc : doc});
 		}
 		else{
 			res.send('<script>alert("실패");history.back();</script>');
 		}
 	});
 
+});
+
+router.post('/delete_trip', function(req, res, next){
+	console.log('req.body = ', req.body);
+	var trip_no = req.body.trip_no;
+	var trip_title = req.body.trip_title;
+	var start_date = req.body.start_date;
+	var end_date = req.body.end_date;
+	var partner_id = req.body.partner_id;
+	var hashtag = req.body.hashtag;
+	var code = 1;
+	var message = "OK";
+	var result = {};
+	var check = {
+		code : code,
+		message : message,
+		result : result
+	};
+
+	TripModel.deleteOne({trip_no : trip_no}, function(err, doc){
+		if(err) {
+			console.log('err =', err);
+			check.code = 0;
+			check.message = err;
+		}
+		if(doc){
+			check.result = doc;
+		}
+		else{
+			check.code = 0;
+			check.message = '실패';
+		}
+		console.log('doc =', doc);
+		res.json(check);
+	});
 });
 
 module.exports = router;
