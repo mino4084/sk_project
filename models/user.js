56820501966219bcaca1d149327ed1d91599492c
mino4084 <mino4084@gmail.com>
2017-08-11 11:00:29 +0900

    여행 생성

--- models/user.js
+++ models/user.js
@@ -21,34 +21,3 @@ UserSchema.plugin(autoIncrement.plugin, { model : 'User', field : 'user_no', sta
 
 var User = db.model('User', UserSchema);
 module.exports = User;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*UserSchema.virtual('myregdate')
-	.get(function(){
-		return formatDate(this.regdate);
-});*/
-
-/*function formatDate(date){
-	var y = date.getFullYear();
-	var m = date.getMonth() + 1;
-	var d = date.getDate();
-	var h = date.getHours();
-	var i = date.getMinutes();
-	var s = date.getSeconds();
-	// yyyy-MM-dd hh:mm:ss 형태
-	var day = y + '-' + (m > 9 ? m : "0" + m) + '-' + (d > 9 ? d : "0" + d) + ' ' + (h > 9 ? h : "0" + h) + ':' + (i > 9 ? i : "0" + i) + ':' + (s > 9 ? s : "0" + s);
-	return day;
-};*/
