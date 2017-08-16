e52581e1d50ee720ce4632611b1c2c4d485ed1d6
mino4084 <mino4084@gmail.com>
2017-08-16 16:09:45 +0900

    후보지 지도 조회

--- routes/index.js
+++ routes/index.js
@@ -751,8 +751,9 @@ router.post('/map_item', function(req, res, next){
 			// console.log('trip_list['+i+'] =', doc.trip_list[i]);
 			if(doc.trip_list[i].schedule_date == schedule_date) {
 				for (var j = 0; j < doc.trip_list[i].schedule_list.length; j++) {
-					if(doc.trip_list[i].schedule_list[j].cate_no == 1){
-						check.result = doc.trip_list[i].schedule_list[j]
+					if(doc.trip_list[i].schedule_list[j].item_placeid == ""){
+						check.result = doc.trip_list[i].schedule_list[j];
+
 					}
 				}
 				console.log('trip_list['+ i +'] =', doc.trip_list[i]);
