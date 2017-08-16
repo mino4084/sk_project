eace25ad76dd21e666a91b1685a526fc642a3016
mino4084 <mino4084@gmail.com>
2017-08-16 16:46:40 +0900

    후보지 지도 조회

--- routes/index.js
+++ routes/index.js
@@ -746,7 +746,7 @@ router.post('/map_item', function(req, res, next){
 	TripModel.findOne({trip_no : trip_no, "trip_list.schedule_date" : schedule_date}, function(err, doc){
 		if(err) return next(err);
 		console.log('list doc =', doc);
-		check.result = doc;
+		//check.result = doc;
 		for(var i = 0; i < doc.trip_list.length; i++) {
 			// console.log('trip_list['+i+'] =', doc.trip_list[i]);
 			if(doc.trip_list[i].schedule_date == schedule_date) {
