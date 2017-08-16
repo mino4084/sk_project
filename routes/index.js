a6f83a66afe17469c0f7d1a8ffe7a59bf551491f
mino4084 <mino4084@gmail.com>
2017-08-16 11:09:03 +0900

    여행 생성 수정

--- routes/index.js
+++ routes/index.js
@@ -367,32 +367,29 @@ router.post('/create_trip', function(req, res, next){
 			check.message = err;
 			return next(err);
 		}
-		check.result = doc;
 		console.log('doc =', doc);
 		console.log('trip_no =', doc.trip_no);
+		//DB에 schedule 생성
+		var day1 = moment(start_date);
+		var day2 = moment(end_date);
+		console.log('day1 =', day1);
+		console.log('day2 =', day2);
+		var num = day2.diff(day1, 'days');
+		console.log('num =', num);
+		for (var i = 1; i <= num + 1; i++) {
+			console.log('i =', i);
+			var scheduleDate = {
+				schedule_date : i
+			};
+			TripModel.findOneAndUpdate({trip_no : doc.trip_no}, {$push : {"trip_list" : scheduleDate}},
+				{safe : true, upsert : true, new : true}, function(err, doc){
+				if(err) return next(err);
+				console.log('schedule update doc =', doc);
+			});
+		}
+		check.result = doc;
 		res.json(check);
 	});
-
-	//DB에 schedule 생성
-	var day1 = moment(start_date);
-	var day2 = moment(end_date);
-	console.log('day1 =', day1);
-	console.log('day2 =', day2);
-	var num = day2.diff(day1, 'days');
-	console.log('num =', num);
-	/*for (var i = 1; i <= num + 1; i++) {
-		console.log('i =', i);
-		var scheduleDate = {
-			schedule_date : i
-		};
-		TripModel.findOneAndUpdate({trip_no : id}, {$push : {"comments" : data}}, {safe : true, upsert : true, new : true}, function(err, doc){
-			if(err) return next(err);
-			console.log('comments update doc =', doc);
-			res.redirect('/blog/' + id);
-		});
-	}*/
-
-
 });
 // 여행 생성
 
