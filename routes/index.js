e00142d07c57b07ffb08c031d2e27ef47bda2586
mino4084 <mino4084@gmail.com>
2017-08-16 16:39:15 +0900

    후보지 지도 조회

--- routes/index.js
+++ routes/index.js
@@ -736,7 +736,7 @@ router.post('/map_item', function(req, res, next){
 	var schedule_date = req.body.schedule_date;
 	var code = 1;
 	var message = "OK";
-	var result = [];
+	var result = new Array();
 	var check = {
 		code : code,
 		message : message,
