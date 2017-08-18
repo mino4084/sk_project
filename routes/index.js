17e4b16a56ca9e390dd2f8d9047a28ef84e99d91
mino4084 <mino4084@gmail.com>
2017-08-18 16:19:54 +0900

    최종일정 리스트 조회

--- routes/index.js
+++ routes/index.js
@@ -1173,9 +1173,8 @@ router.post('/list_final', function(req, res, next){
 				// arr.schedule_list.splice(i, 1);
 			}
 		};*/
-		console.log('arr = ', arr);
-		console.log('arr.schedule_list.length = ', arr.schedule_list.length);
-		for(var i = arr.schedule_list.length; i >= 0; i--) {
+
+		for(var i = arr.schedule_list.length-1; i >= 0; i--) {
 			//console.log('arr.schedule_list[i].item_check =', arr.schedule_list[i].item_check);
 			console.log('arr.schedule_list[i] =', arr.schedule_list[i]);
 			// console.log('arr.schedule_list[i] =', arr.schedule_list[i]);
