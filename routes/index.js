cbd2f64cd84b38a86953b54612949e7192f375e0
mino4084 <mino4084@gmail.com>
2017-08-18 12:05:38 +0900

    최종일정 리스트 조희

--- routes/index.js
+++ routes/index.js
@@ -1149,9 +1149,10 @@ router.post('/list_final', function(req, res, next){
 			};
 		};
 		for(var i = 0; i < arr.schedule_list.length; i++) {
-			if(arr.schedule_list[i].item_check == 0){
+			console.log('arr.schedule_list[i] =', arr.schedule_list[i]);
+			/*if(arr.schedule_list[i].item_check == 0){
 				arr.schedule_list[i].splice(i, 1);
-			}
+			}*/
 		};
 		console.log('arr =', arr);
 		res.json(check);
