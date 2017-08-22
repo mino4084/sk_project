3cc699d239e08d9fef1075334e68a4b072875a7a
mino4084 <mino4084@gmail.com>
2017-08-22 18:04:54 +0900

    알림

--- routes/index.js
+++ routes/index.js
@@ -580,7 +580,7 @@ router.post('/list_trip', function(req, res, next){
 
 	var code = 1;
 	var message = "OK";
-	var result = [];
+	var result = {};
 	var check = {
 		code : code,
 		message : message,
