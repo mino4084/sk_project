092746342c81e9513e5de8b7edab3e1af22dfd7d
mino4084 <mino4084@gmail.com>
2017-08-23 12:38:08 +0900

    여행 수정

--- routes/index.js
+++ routes/index.js
@@ -289,7 +289,7 @@ router.post('/check_pw', function(req, res, next){
 
 	var code = 1;
 	var message = "OK";
-	var result = [];
+	var result = {};
 	var check = {
 		code : code,
 		message : message,
@@ -329,7 +329,7 @@ router.post('/change_pw', function(req, res, next){
 	var user_pw = req.body.user_pw;
 	var code = 1;
 	var message = "OK";
-	var result = [];
+	var result = {};
 	var check = {
 		code : code,
 		message : message,
@@ -368,7 +368,7 @@ router.post('/stop', function(req, res, next){
 	var stop = 1;
 	var code = 1;
 	var message = "OK";
-	var result = [];
+	var result = {};
 	var check = {
 		code : code,
 		message : message,
