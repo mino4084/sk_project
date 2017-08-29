ae5cdcffbc2a2d3777fffac35cef2bbf05b8f973
mino4084 <mino4084@gmail.com>
2017-08-29 13:53:41 +0900

    후보지 생성

--- routes/index.js
+++ routes/index.js
@@ -1038,7 +1038,7 @@ router.post('/create_item', function(req, res, next){
 						};
 					};// for
 					console.log('num =', num);
-					data.item_title = '위치' + num;
+					data.item_title = '위치' + (num + 1);
 				}
 				console.log('data.item_title =', data.item_title);
 				console.log('user_token =', doc2.user_token);
@@ -1096,7 +1096,7 @@ router.post('/create_item', function(req, res, next){
 					for(var i = 0; i < doc.trip_list.length; i++) {
 						if(doc.trip_list[i].schedule_date == schedule_date) {
 							for (var j = 0; j < doc.trip_list[i].schedule_list.length; j++) {
-								if(doc.trip_list[i].schedule_list[j].item_title == null){
+								if(doc.trip_list[i].schedule_list[j].item_title !== null){
 									num++;
 								}
 							}
