aeff11b3e92b536046e9ea5783b1f2b5c545380b
mino4084 <mino4084@gmail.com>
2017-08-31 12:34:00 +0900

    프로필 사진 변경

--- routes/index.js
+++ routes/index.js
@@ -2428,9 +2428,10 @@ router.post('/list_notice', function(req, res, next){
 				}
 				async.eachSeries(docs2, function(item2, callbackIn2){ // 반복
 					// arr.push(item2);
-					if(user_id !== item2.notice_partner){
+					/*if(user_id !== item2.notice_partner){
 						arr.push(item2);
-					}
+					}*/
+					arr.push(item2);
 					callbackIn2();
 				}, function(err){ // eachSeries 완료
 					if(err){
